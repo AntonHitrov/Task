f53f9d571097e769e2906fe5b65a7e52fee8b610
AntonHitrov <hitrov.anton.vladimirovich@gmail.com>
2022-02-17 17:32:39 +0400

    Refact

--- Task/Assets/Scripts/Line.cs
+++ Task/Assets/Scripts/Line.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 class Line
 {
     internal readonly float A, B, C, D;
-    internal readonly Vector2  v1, v2, Perpendicular, normal;
+    internal readonly Vector2  v1, v2/*, Perpendicular/*,normal*/;
 
     internal Line(Vector2 v1, Vector2 v2)
     {
@@ -15,12 +15,9 @@ class Line
         A = (v1.y - v2.y) / D;
         B = (v2.x - v1.x) / D;
         C = ((v1.x * v2.y) - (v2.x * v1.y)) / D;
-        normal = (v2 - v1).normalized;
-        Perpendicular = Vector2.Perpendicular(v2 - v1).normalized;
+        /*normal = (v2 - v1).normalized;*/
+       /* Perpendicular = Vector2.Perpendicular(v2 - v1).normalized;*/
     }
-
-
-    internal IEnumerable<Vector2> Positions() { yield return v1; yield return v2; }
 
     internal float Scale(Vector2 position) => (A * position.x) + (B * position.y) + C;
 
